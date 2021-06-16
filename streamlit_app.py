44dd57904b0e0e5754320e9c197faf4f27ae1a9f
Joseph Angelo Perez <79298332+jsphprz@users.noreply.github.com>
2021-06-16 12:27:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import bitly_api
 
-token = "49c4a71e8309dd60d68c3c4d3ad80e2c7021cf77"
-connection = bitly_api.Connection(access_token=token)
+c = bitly_api.Connection(access_token='49c4a71e8309dd60d68c3c4d3ad80e2c7021cf77')
 
 st.set_page_config(
         page_title="URL Shortener",
@@ -21,5 +20,5 @@ with body:
 with st.form('input_url'):
     url = st.text_input(label = "Enter URL:")
     if st.form_submit_button('Confirm'):
-        shorten_url = connection.shorten(url)
+        shorten_url = c.shorten(url)
         st.success("Shortened URL: " + "**" + shorten_url.get('url') + "**")
