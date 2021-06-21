8f6b9345576a78973866f8926ad7b8b85b7c30ff
unknown <josephperez279@gmail.com>
2021-06-21 21:52:19 +0800

    updated code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import bitly_api
+from PIL import Image
+import qrcode
+import cv2
 
-token = "49c4a71e8309dd60d68c3c4d3ad80e2c7021cf77"
-connection = bitly_api.Connection(access_token=token)
+#token = "49c4a71e8309dd60d68c3c4d3ad80e2c7021cf77"
+c = bitly_api.Connection(access_token='49c4a71e8309dd60d68c3c4d3ad80e2c7021cf77')
+d = cv2.QRCodeDetector()
+#connection = bitly_api.Connection(access_token=token)
 
 st.set_page_config(
         page_title="URL Shortener",
@@ -21,5 +26,11 @@ with body:
 with st.form('input_url'):
     url = st.text_input(label = "Enter URL:")
     if st.form_submit_button('Confirm'):
-        shorten_url = connection.shorten(url)
+        shorten_url = c.shorten(url)
         st.success("Shortened URL: " + "**" + shorten_url.get('url') + "**")
+        col1, col2, col3, col4, col5, col6 = st.beta_columns([1,6,1,6,1,6])
+        img = qrcode.make(url)
+        img.save("url.jpg")
+        image = Image.open("url.jpg")
+        with col3:
+            st.image(image, caption="QR Code", width=200, output_format='auto')
