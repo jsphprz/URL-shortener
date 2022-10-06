d6df59d55b9d9dd0e1b6646696adebf7aa42c52d
Bojo <josephperez279@gmail.com>
2022-10-06 09:29:37 +0800

    fixed app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,17 @@ import bitly_api
 from PIL import Image
 import qrcode
 
-<<<<<<< HEAD
-#token = "49c4a71e8309dd60d68c3c4d3ad80e2c7021cf77"
-c = bitly_api.Connection(access_token='49c4a71e8309dd60d68c3c4d3ad80e2c7021cf77')
-#connection = bitly_api.Connection(access_token=token)
-=======
+#token = "7d2d5ab75fb4104d391501975235e0b3f1d55045"
 c = bitly_api.Connection(access_token='7d2d5ab75fb4104d391501975235e0b3f1d55045')
->>>>>>> 50c3ef4f951413873fa6ed76c04c0ed64b5befd5
+#connection = bitly_api.Connection(access_token=token)
 
 st.set_page_config(
         page_title="URL Shortener",
         page_icon="🔗"
     )
 
-header = st.beta_container()
-body = st.beta_container()
+header = st.container()
+body = st.container()
 
 with header:
     st.title('URL Shortener 🔗')
@@ -30,7 +26,7 @@ with st.form('input_url'):
     if st.form_submit_button('Confirm'):
         shorten_url = c.shorten(url)
         st.success("Shortened URL: " + "**" + shorten_url.get('url') + "**")
-        col1, col2, col3, col4, col5, col6 = st.beta_columns([1,6,1,6,1,6])
+        col1, col2, col3, col4, col5, col6 = st.columns([1,6,1,6,1,6])
         img = qrcode.make(url)
         img.save("url.jpg")
         image = Image.open("url.jpg")
